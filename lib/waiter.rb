0b36f712712df2cc2384d8fc320b734d7868bebe
Hannah Angle <hannahcangle@gmail.com>
2020-08-12 03:28:16 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -17,11 +17,6 @@ attr_accessor :name, :yrs_experience
     Meal.new(self, customer, total, tip)
   end
   
-  def meals
-  Meal.all.select do |meal|
-    meal.customer == self
-  end
-end
 
 def waiters
   meals.map do |meal|
